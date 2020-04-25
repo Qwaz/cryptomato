24b1d266b80d1e5a128c32c441e27a08355b4066
Yechan Bae <yechan@gatech.edu>
2020-04-25 01:47:38 -0400

    Update grpc script

--- worker/generate_grpc.sh
+++ worker/generate_grpc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./venv/bin/python -m grpc_tools.protoc -I./protos --python_out=./exposed_lib --grpc_python_out=./exposed_lib ./protos/api.proto
-./venv/bin/python -m grpc_tools.protoc -I./protos --python_out=./protos --grpc_python_out=./protos ./protos/private_api.proto
-ln -s ../exposed_lib/api_pb2.py protos
-ln -s ../exposed_lib/api_pb2_grpc.py protos
+/usr/bin/env python -m grpc_tools.protoc -I./protos --python_out=./exposed_lib --grpc_python_out=./exposed_lib ./protos/api.proto
+/usr/bin/env python -m grpc_tools.protoc -I./protos --python_out=./protos --grpc_python_out=./protos ./protos/private_api.proto
+ln -sf ../exposed_lib/api_pb2.py protos
+ln -sf ../exposed_lib/api_pb2_grpc.py protos
